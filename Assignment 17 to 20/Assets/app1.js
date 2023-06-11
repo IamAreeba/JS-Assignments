2e98e7ae41fb175afba98c46fbf0626aef5f30d9
IamAreeba <117988518+IamAreeba@users.noreply.github.com>
2023-06-11 09:14:51 -0700

    Add files via upload

---
+++ Assignment 17 to 20/Assets/app1.js
@@ -0,0 +1,114 @@
+// Question 1:
+let arr1 = [[1,2], ["Areeba, Amjad"]];
+
+
+// Question 2:
+// let arr2 = [[0, 1, 2, 3], [1, 0, 1, 2], [2, 1, 0, 1]];
+
+// Question 3:
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+//   }
+  
+// Question 4:
+// let tabNum = parseInt(prompt("Enter the number for multiplication table:"));
+// let tabLen = parseInt(prompt("Enter the length of the multiplication table:"));
+
+// console.log(`Multiplication Table of ${tableNumber}:`);
+// let result;
+// for (let i = 1; i <= tableLength; i++) {
+//   result = tableNumber * i;
+//   console.log(`${tableNumber} x ${i} = ${result}`);
+// }
+
+
+// Question 5:
+// let fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+// console.log("Items of the fruits array:");
+// for (let i = 0; i < fruits.length; i++) {
+//   console.log(fruits[i]);
+// }
+
+// Question 6:
+// Counting: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+
+// console.log("Counting:");
+// for (let i = 1; i <= 15; i++) {
+//   console.log(i);
+// }
+
+// Reverse counting: 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
+// console.log("Reverse counting:");
+// for (let i = 10; i >= 1; i--) {
+//   console.log(i);
+// }
+
+// Even: 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20
+// console.log("Even:");
+// for (let i = 0; i <= 20; i += 2) {
+//   console.log(i);
+// }
+
+// Odd: 1, 3, 5, 7, 9, 11, 13, 15, 17, 19
+// console.log("Odd:");
+// for (let i = 1; i <= 19; i += 2) {
+//   console.log(i);
+// }
+
+// Series: 2k, 4k, 6k, 8k, 10k, 12k, 14k, 16k, 18k, 20k
+// console.log("Series:");
+// for (let i = 2; i <= 20; i += 2) {
+//   console.log(i + "k");
+// }
+
+
+// Question 7:
+// let found = false;
+// for (let i = 0; i < A.length; i++) {
+//   if (A[i] === userInput) {
+//     found = true;
+//     break;
+//   }
+// }
+
+// if (found) {
+//   alert(`Yes, '${userInput}' is  in the list.`);
+// } else {
+//   alert(`No, '${userInput}' is not  in the list.`);
+// }
+
+
+// Question 8:
+
+// let Aa = [24, 53, 78, 91, 12];
+// document.write("Array items: ");
+// document.write(Aa);
+// let num = 0;
+// for (let i = 0; i < Aa.length; i++) {
+//   if (Aa[i] > num) {
+//     num = Aa[i];
+//   }
+// }
+// document.write("<br>The largest number is "+num);
+
+// Question 9:
+
+// let Ab = [24, 53, 78, 91, 12];
+// document.write("Array items: ");
+// document.write(Ab);
+// let num1 = Ab[0];
+// for (let i = 0; i < Ab.length; i++) {
+//   if (Ab[i] < num1) {
+//     num1 = Ab[i];
+//   }
+// }
+// document.write("<br>The smallest number is "+num1);
+
+// Question 10:
+
+// console.log("Multiples of 5 from 1 to 100:");
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 5 === 0) {
+//     console.log(i);
+//   }
+// }
